d92525d72ac2b2f0b157f02548babfc22269e5ca
leftshift <leftshiftlp@gmail.com>
2014-10-25 23:18:31 +0200

    first draft of sidebar

--- js/app.js
+++ js/app.js
@@ -1,4 +1,8 @@
 $("#reload").on("click", function(){
   console.log("Reloading")
-  location.reload()
+  location.reload(true)
+})
+$("#dev-btn").on("click", function(){
+  console.log("Opening dev tools")
+  require('nw.gui').Window.get().showDevTools()
 })
