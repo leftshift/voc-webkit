435d99c55c23f2790a4af1022c18c6a9fc0ef206
leftshift <leftshiftlp@gmail.com>
2014-10-26 20:13:43 +0100

    Added some Json-parsing for testing

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+var fs = require('fs');
+
+var voc_dir = "./voc/"
+
 //Dev buttons and such
 $("#reload").on("click", function(){
   console.log("Reloading")
@@ -16,13 +20,49 @@ $("#dev-add-sidebar-item").on("click", function() {
   //get the on click call
 })
 
+//get files in directory for the voc files (TODO: Where should the files go?)
+fs.readdir(voc_dir, populateSidebar)
+
 //Highlight selected sidebar item
 //Hacky
 makeSidebarClickable()
+
 function makeSidebarClickable(){
-  $(".sidebar a").on("click", function(){
+  $(".sidebar ul a").on("click", function(){
     console.log("clicked on " + $(this).data("path"))
     $(".sidebar li").removeClass("active")
     $(this).children("li").addClass("active")
+    populateContent($(this).data("path"))
   })
+}
+
+function populateSidebar(err, files) {
+  console.log("Populating sidebar")
+  console.log(err)
+  console.log(files)
+  if (files) {
+    //clear current list
+    $(".sidebar ul a").remove()
+
+    for (var i=0; i < files.length; i++) {
+      console.log("file " + files[i])
+      $(".sidebar ul").append('<a href="#" data-path="' + voc_dir + files[i] +'"><li>'+ files[i] +'</li></a>')
+    }
+
+    makeSidebarClickable()
+  } else {
+    console.log("no files")
+
+  }
+}
+
+function populateContent(file) {
+  console.log("Populating content with " + file)
+  fs.readFile(file, "utf8",populateEditTable)
+}
+
+function populateEditTable(err, data) {
+
+  console.log(err)
+  console.log(JSON.parse(data))
 }
