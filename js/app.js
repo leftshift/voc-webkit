e1634897ed705455c9de0b0f2dd9b2fafd7197dc
leftshift <leftshiftlp@gmail.com>
2014-10-26 14:59:06 +0100

    Added sidebar

--- js/app.js
+++ js/app.js
@@ -6,3 +6,9 @@ $("#dev-btn").on("click", function(){
   console.log("Opening dev tools")
   require('nw.gui').Window.get().showDevTools()
 })
+
+$(".sidebar a").on("click", function(){
+  console.log("clicked on " + $(this).data("path"))
+  $(".sidebar li").removeClass("active")
+  $(this).children("li").addClass("active")
+})
