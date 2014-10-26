825f34d499963c4d06ca789389c648dff213b3a7
leftshift <leftshiftlp@gmail.com>
2014-10-26 16:16:58 +0100

    Various layout fixes

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+//Dev buttons and such
 $("#reload").on("click", function(){
   console.log("Reloading")
   location.reload(true)
@@ -7,8 +8,21 @@ $("#dev-btn").on("click", function(){
   require('nw.gui').Window.get().showDevTools()
 })
 
-$(".sidebar a").on("click", function(){
-  console.log("clicked on " + $(this).data("path"))
-  $(".sidebar li").removeClass("active")
-  $(this).children("li").addClass("active")
+$("#dev-add-sidebar-item").on("click", function() {
+  console.log("Adding item to sidebar")
+  $(".sidebar ul").append('<a href="#" data-path="test/"><li>Lulz</li></a>')
+  makeSidebarClickable()
+  //Works, i guess? Otherwise, the newly added items don't
+  //get the on click call
 })
+
+//Highlight selected sidebar item
+//Hacky
+makeSidebarClickable()
+function makeSidebarClickable(){
+  $(".sidebar a").on("click", function(){
+    console.log("clicked on " + $(this).data("path"))
+    $(".sidebar li").removeClass("active")
+    $(this).children("li").addClass("active")
+  })
+}
